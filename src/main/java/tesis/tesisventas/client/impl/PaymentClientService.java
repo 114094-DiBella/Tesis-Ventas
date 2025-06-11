e6625d794330ff14c01f3aa1834beb913336ad80
agustin <114094@tecnicatura.frc.utn.edu.ar>
2025-06-10 23:43:49 -0300

    Dockerfile

--- src/main/java/tesis/tesisventas/client/impl/PaymentClientService.java
+++ src/main/java/tesis/tesisventas/client/impl/PaymentClientService.java
@@ -1,28 +1,38 @@
 package tesis.tesisventas.client.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import tesis.tesisventas.dtos.PaymentRequest;
+import tesis.tesisventas.services.impl.FacturaServiceImpl;
 
 import java.util.Map;
 
 @Service
 public class PaymentClientService {
 
-    private final String PAYMENT_SERVICE_URL = "http://localhost:8082";
+    private static final Logger logger = LoggerFactory.getLogger(PaymentClientService.class);
+
+    @Value("${services.payment.url}")
+    private String PAYMENT_SERVICE_URL;
 
     @Autowired
     private RestTemplate restTemplate;
 
     public String createPaymentPreference(PaymentRequest request) {
+        logger.info("Create payment preference");
         String url = PAYMENT_SERVICE_URL + "/api/payments/create-preference";
-
+        logger.info("URL: {}", url);
         try {
             ResponseEntity<Map> response = restTemplate.postForEntity(url, request, Map.class);
+            logger.info("Response: {}", response.getBody());
             return (String) response.getBody().get("preferenceId");
         } catch (Exception e) {
+            logger.error("Error creating payment preference", e);
             throw new RuntimeException("Error al crear preferencia de pago", e);
         }
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO forma_pago (id, name, active)
-VALUES ('11111111-1111-1111-1111-111111111111', 'Efectivo', true);
+VALUES ('11111111-1111-1111-1111-111111111111', 'Efectivo', 1);
 
 INSERT INTO forma_pago (id, name, active)
-VALUES ('5ef1b016-64ff-4f2f-973c-cffd9a39c30e', 'Trasnfer', true);
+VALUES ('5ef1b016-64ff-4f2f-973c-cffd9a39c30e', 'Trasnfer', 1);
 
 INSERT INTO forma_pago (id, name, active)
-VALUES ('22222222-2222-2222-2222-222222222222', 'mp', true);
+VALUES ('22222222-2222-2222-2222-222222222222', 'mp', 1);
 
 
 
